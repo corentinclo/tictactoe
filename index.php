cd52391e64ca2f9ace01f38127cfcbe49ee3c3ef
corentinclo <corentincloarec@hotmail.fr>
2015-07-15 20:21:24 +0200

    commit

--- index.php
+++ index.php
@@ -4,22 +4,27 @@
   <meta charset="utf-8">
   <title>Titre de la page</title>
   <link rel="stylesheet" href="style.css">
-  <script src="main.js"></script>
+  <link rel="stylesheet" href="site.min.css">
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="main.js"></script>
 </head>
-<body onload="init();">
+<body>
  
-    <div id="game">
-     	<canvas class="case" id="1" width="150" height="150" onclick="click(this);"></canvas>	
-     	<canvas class="case" id="2" width="150" height="150"></canvas>	
-     	<canvas class="case" id="3" width="150" height="150"></canvas>	
-     	<canvas class="case" id="4" width="150" height="150"></canvas>	
-     	<canvas class="case" id="5" width="150" height="150"></canvas>	
-     	<canvas class="case" id="6" width="150" height="150"></canvas>	
-     	<canvas class="case" id="7" width="150" height="150"></canvas>	
-     	<canvas class="case" id="8" width="150" height="150"></canvas>	
-     	<canvas class="case" id="9" width="150" height="150"></canvas>	
+	<div id="title">
+		<h1>Tic-Tac-Toe</h1>
+		<button id="reset_button">Reset</button>
+	</div>
 
-    </div>	
+    <div id="game">
+     	<canvas class="case" id="1"></canvas>	
+     	<canvas class="case" id="2"></canvas>	
+     	<canvas class="case" id="3"></canvas>	
+     	<canvas class="case" id="4"></canvas>	
+     	<canvas class="case" id="5"></canvas>		
+     	<canvas class="case" id="6"></canvas>	
+     	<canvas class="case" id="7"></canvas>		
+     	<canvas class="case" id="8"></canvas>	
+     	<canvas class="case" id="9"></canvas>
+    </div>
 </body>
 </html>
